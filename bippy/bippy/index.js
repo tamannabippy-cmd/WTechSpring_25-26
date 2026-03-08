9e12bc2752f30481c31918bbca253fe4d88bfb8d
tamannabippy-cmd <tamannabippy@gmail.com>
2026-03-08 20:50:11 +0600

    file added

---
+++ bippy/bippy/index.js
@@ -0,0 +1,9 @@
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const message = document.getElementById('message');
+
+if (fname != "Bippi") {
+    alert("Please enter your right name");
+}
